e471d2799a4367d4de47ca049fb76124d839fed0
Maxim <zinovev.maxim93@mail.ru>
2020-08-18 04:18:44 +0300

    Основа готова, документация в работе

--- app.py
+++ app.py
@@ -1,42 +1,85 @@
-#!/home/maxim/projects/FAPIenv/bin/python
-from flask import Flask, jsonify, make_response
+#!/usr/bin/python
+from flask import Flask, jsonify, make_response, request, abort
 from models import User
 import json
 
 app = Flask(__name__)
 
 
-@app.route('/get_users', methods=['GET'])
-def get_users():
+@app.route('/users', methods=['GET'])
+def api_get_users():
     return jsonify({'users': User.get_users()})
 
 
-@app.route('/get_user_by_id/<int:user_id>', methods=['GET'])
-def get_user_by_id(user_id):
+@app.route('/users/<int:user_id>', methods=['GET'])
+def api_get_user_by_id(user_id):
     user = User.get_user_by_id(user_id)
     if user:
-        return jsonify({'user': user})
+        return jsonify({'user': user[0]})
+
     abort(404)
 
 
 @app.route('/create_user', methods=['POST'])
-def create_user(user_id, name):
-    pass
+def api_create_user():
+    if not request.json or not 'name' in request.json:
+        abort(400)
+
+    if 'age' in request.json:
+        new_user = User(request.json['name'], request.json['age'])
+    else:
+        new_user = User(request.json['name'])
+    new_user.create_user()
+    return jsonify({'result': True})
+
+@app.route('/users/<int:user_id>', methods=['DELETE'])
+def api_delete_user(user_id):
+
+    user = User.get_user_by_id(user_id)
+    all_users = User.get_users()
+    if not user:
+        abort(404)
+
+    all_users.remove(user[0])
+    User.write_in_file_users(all_users)
+    return jsonify({'result': True})
 
 
-# @app.route('/delete_user', methods=['DELETE'])
-# def delete_user():
-#     pass
+@app.route('/users/<int:user_id>', methods=['PUT'])
+def api_update_user(user_id):
 
+    user = User.get_user_by_id(user_id)
+    all_users = User.get_users()
 
-@app.route('/change_user', methods=['PUT'])
-def delete_user(user_id):
-    pass
+    if not user:
+        abort(404)
+    if not request.json:
+        abort(400)
+
+    if 'name' in request.json:
+        if type(request.json['name']) != str:
+            abort(400)
+        all_users[all_users.index(user[0])]['name'] = request.json['name']
+        User.write_in_file_users(all_users)
+
+    if 'age' in request.json:
+        if type(request.json['age']) != int:
+            abort(400)
+
+        all_users[all_users.index(user[0])]['age'] = request.json['age']
+        User.write_in_file_users(all_users)
+
+    return jsonify({'result': True})
 
 
 @app.errorhandler(404)
 def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
+
+
+@app.errorhandler(400)
+def bad_request(error):
+    return make_response(jsonify({'error': 'Bad Request'}), 400)
 
 
 if __name__ == '__main__':
--- models.py
+++ models.py
@@ -1,43 +1,71 @@
 from datetime import datetime
+from typing import Optional, Union
 
 import json, os
 
 class User(object):
+    """Класс пользователя, принимает в качестве полей:
+    1. Имя пользователя (str) - обязательный
+    2. Возраст (int) - опционально, можно не вводить, тогда будет None"""
 
-    def __init__(self, id: int, name: str):
-        self.id = id
+    def __init__(self, name: str, age: Optional[int]=None):
+        self.id = self.get_new_id
         self.name = name
+        self.age = age
 
 
     def __str__(self):
-        return f'{self.name} ({self.id})'
+        return f'{self.name} (id: {self.id})'
+
+
+    @property
+    def get_new_id(self):
+        """id_counter содержит последний присвоенный id
+        Считать последний присвоенный id
+        Присвоить новому пользователю id + 1
+        Записать новый id в файл"""
+        with open('id_counter', 'r') as read_last_id:
+            id = int(read_last_id.read()) + 1
+        with open('id_counter', 'w') as write_new_last_id:
+            write_new_last_id.write(str(id))
+        return id
+
+
+    @staticmethod
+    def write_in_file_users(data):
+        with open('users.json', 'w') as dump_json:
+            json.dump(data, dump_json)
 
 
     @staticmethod
     def get_users():
         """Возвращает список всех пользователей"""
-        with open('FlaskAPI/users.json', 'r') as load_file:
+        with open('users.json', 'r') as load_file:
             return json.load(load_file)
 
 
     @staticmethod
-    def get_user_by_id(user_id):
-        with open('FlaskAPI/users.json', 'r') as load_file:
+    def get_user_by_id(user_id: int):
+        """Возвращает пользователя по его id"""
+        with open('users.json', 'r') as load_file:
             return [user for user in json.load(load_file) \
-                if user['id'] == user_id][0]
+                if user['id'] == user_id]
 
 
     def create_user(self):
+        """Создаёт нового пользователя"""
+
         with open('users.json', 'r') as load_file:
             data = json.load(load_file)
+
         user_data = {
             'id': self.id,
             'name': self.name,
+            'age': self.age,
             'creation_datetime': datetime.now().strftime('%d.%m.%Y %H:%M'),
-            'last_change_datetime': None
+            'last_update_datetime': None
         }
 
         data.append(user_data)
 
-        with open('users.json', 'w') as dump_json:
-            json.dump(data, dump_json)
+        self.write_in_file_users(data)
